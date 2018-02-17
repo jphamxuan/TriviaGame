3999f0aa92c73c99eb0ef6773eb5023250f9f4ca
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-02-17 01:19:51 -0800

    new game folder

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var triviaQs = [
+        {
+            question: "Who is the 2 time?",
+            choice: ["Summit1G", "Dr. Disrespect", "Tyler", "Your Mom"],
+            answer: "Dr. Disrespect"
+
+
+
+        }
+    ]
+
+
+}];
+    
